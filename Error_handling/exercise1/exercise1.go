7af3ed0947669f73a53928fc1a9016bff205fd11
hari.mj <hari.mj@incedoinc.com>
2020-06-05 21:36:19 +0530

    error 1

---
+++ Error_handling/exercise1/exercise1.go
@@ -0,0 +1,70 @@
+// Create two error variables, one called ErrInvalidValue and the other
+// called ErrAmountTooLarge. Provide the static message for each variable.
+// Then write a function called checkAmount that accepts a float64 type value
+// and returns an error value. Check the value for zero and if it is, return
+// the ErrInvalidValue. Check the value for greater than $1,000 and if it is,
+// return the ErrAmountTooLarge. Write a main function to call the checkAmount
+// function and check the return error value. Display a proper message to the screen.
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Add imports.
+
+var (
+	// Declare an error variable named ErrInvalidValue using the New
+	// function from the errors package.
+	ErrInvalidValue = errors.New("Invalid Value")
+
+	// Declare an error variable named ErrAmountTooLarge using the New
+	// function from the errors package.
+	ErrAmountTooLarge = errors.New("Amount Too Large")
+)
+
+// Declare a function named checkAmount that accepts a value of
+// type float64 and returns an error interface value.
+func checkAmount(f float64) error {
+
+	switch {
+
+	// Is the parameter equal to zero.
+	case f == 0:
+		return ErrInvalidValue
+
+	// Is the parameter greater than 1000.
+	case f > 1000:
+		return ErrAmountTooLarge
+	}
+
+	return nil
+}
+
+func main() {
+
+	// Call the function and get the error.
+	if err := checkAmount(0); err != nil {
+		switch err {
+
+		// Check if the error is an ErrInvalidValue.
+		case ErrInvalidValue:
+			fmt.Println("Value provided is not valid.")
+			return
+
+		// Check if the error is an ErrAmountTooLarge.
+		case ErrAmountTooLarge:
+			fmt.Println("Value provided is too large.")
+			return
+
+		// Handle the default error.
+		default:
+			fmt.Println(err)
+			return
+		}
+	}
+
+	// Display everything is good.
+	fmt.Println("Everything checks out.")
+}
